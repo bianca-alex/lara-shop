4c7c9d6c4a2010ac711249f519db10abe539f1f9
bianca-alex <bianca.lk.alex@outlook.com>
2021-03-13 17:15:39 +0800

    新增收货地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 
 /**
  * The following block of code may be used to automatically register your
