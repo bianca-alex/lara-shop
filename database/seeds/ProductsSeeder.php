89822b5faebb59d40ed505550873a22390af87ef
bianca-alex <bianca.lk.alex@outlook.com>
2021-03-14 14:35:14 +0800

    用户界面 - 商品列表

---
+++ database/seeds/ProductsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\ProductSku;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $products = factory(Product::class, 30)->create();
+        foreach($products as $product){
+            $skus = factory(ProductSku::class, 3)->create(['product_id' => $product->id]);
+            $product->update(['price' => $skus->min('price')]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('/products', 'ProductsController@index')->name('products.index');
 
 Auth::routes(['verify' => true]);
 
