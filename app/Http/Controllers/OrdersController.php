55a33c7d6cb2d8a5ea1511fe35890ae21cd54545
bianca-alex <bianca.lk.alex@outlook.com>
2021-03-21 14:03:49 +0800

    用户界面 - 订单详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -24,6 +24,11 @@ class OrdersController extends Controller
         return view('orders.index', ['orders' => $orders]);
     }
 
+    public function show(Request $request, Order $order){
+        $this->authorize('own', $order);
+        return view('orders.show', ['order' => $order->load(['items.product', 'items.productSku'])]);
+    }
+
     //
     public function store(OrderRequest $request)
     {
