6f91caa5d452e0240b4f4ea472058b0daad7c8bd
bianca-alex <bianca.lk.alex@outlook.com>
2021-03-22 22:46:48 +0800

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -20,4 +20,5 @@ Route::group([
     $router->delete('products/{id}', 'ProductsController@destroy');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
