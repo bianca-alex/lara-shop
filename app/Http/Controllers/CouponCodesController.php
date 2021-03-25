77f61bd36a5fc88fd3df43c851dd8b78f7b76c92
bianca-alex <bianca.lk.alex@outlook.com>
2021-03-25 22:06:28 +0800

    用户界面 - 使用优惠券下单

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\CouponCode;
+use App\Exceptions\CouponCodeUnavailableException;
 
 class CouponCodesController extends Controller
 {
@@ -12,23 +13,10 @@ class CouponCodesController extends Controller
 
     public function show($code){
         if(!$record = CouponCode::where('code', $code)->first()){
-            abort(404);
-        }
-        
-        if(!$record->enabled){
-            abort(404);
+            throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        if($record->total - $record->used <= 0){
-            return response()->json(['msg' => '该优惠卷已兑完'], 403);
-        }
-
-        if($record->not_before && $record->not_before->gt(Carbon::now())){
-            return response()->json(['msg' => '该优惠券还不能使用'], 403);
-        }
-        if($record->not_after && $record->not_after->lt(Carbon::now())){
-            return response()->json(['msg' => '该优惠券已经过期'], 403);
-        }
+        $record->checkAvailable();
 
         return $record;
     }
